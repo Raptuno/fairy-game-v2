8470df8eb3fb723e6273e9b00f7a4d4e4a6ee8cd
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-02-11 19:46:56 -0600

    Creation

---
+++ OP-FinalCH/src/core/SwtMain.java
@@ -0,0 +1,51 @@
+package core;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FormLayout;
+
+public class SwtMain {
+
+	protected Shell shell;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			SwtMain window = new SwtMain();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		shell.setLayout(new FormLayout());
+
+	}
+
+}
---
+++ OP-FinalCH/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author RaptunoCyborg
+ *
+ */
+module modulara {
+	requires org.eclipse.swt.win32.win32.x86_64;
+}
